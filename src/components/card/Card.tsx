a7890d7bc7c96f00f77a909a628f84701475ac1c
Doriane <berdalado@gmail.com>
2022-10-12 17:27:44 +0200

    card component

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,59 @@
+import {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useContext,
+  useState,
+} from "react";
+import "./Card.css";
+import { UserContext } from "../../contexts/UserContext";
+
+interface CardProps {
+  selectedUsers: number[];
+  setSelectedUsers: Dispatch<SetStateAction<number[]>>;
+}
+interface User {
+  avatar_url: string;
+  id: number;
+  login: string;
+}
+const Card = ({ selectedUsers, setSelectedUsers }: CardProps) => {
+  const [isSelected, setIsSelected] = useState(false);
+  const { searchResults } = useContext(UserContext);
+
+  const handleClick = useCallback(
+    (id: number) => {
+      if (!selectedUsers.includes(id)) {
+        setIsSelected(!isSelected);
+        setSelectedUsers([...selectedUsers, id]);
+        console.log("passe la ");
+      } else {
+        setIsSelected(!isSelected);
+        setSelectedUsers(
+          selectedUsers.filter((savedId: number) => savedId !== id)
+        );
+        console.log("passe ici et là ");
+      }
+    },
+    [isSelected, selectedUsers]
+  );
+  return (
+    <div className='list'>
+      {searchResults.map(({ avatar_url, id, login }: User) => (
+        <div className='card card-shadow'>
+          <input type='checkbox' onClick={() => handleClick(id)}></input>
+          <div className='card-avatar'>
+            <img src={avatar_url} alt='avatar' />
+          </div>
+          <div className='card-body'>{id}</div>
+          <div className='card-body'>{login}</div>
+          <div className='card-footer'>
+            <button className='btn'>View profile</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Card;
--- src/components/userList/UserList.tsx
+++ src/components/userList/UserList.tsx
@@ -1,34 +1,11 @@
-import { useCallback, useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../../contexts/UserContext";
+import Card from "../card/Card";
 import "./UserList.css";
 
-interface UserProps {
-  avatar_url: string;
-  id: number;
-  login: string;
-}
-
 const UserList = () => {
-  const [isSelected, setIsSelected] = useState<boolean>(false);
   const [selectedUsers, setSelectedUsers] = useState<any[]>([]);
   const { searchResults, errors } = useContext(UserContext);
-
-  const handleClick = useCallback(
-    (id: number) => {
-      if (!selectedUsers.includes(id)) {
-        setIsSelected(!isSelected);
-        setSelectedUsers([...selectedUsers, id]);
-        console.log("passe la ");
-      } else {
-        setIsSelected(!isSelected);
-        setSelectedUsers(
-          selectedUsers.filter((savedId: number) => savedId !== id)
-        );
-        console.log("passe ici et là ");
-      }
-    },
-    [isSelected, selectedUsers]
-  );
 
   return !searchResults || errors ? (
     <span style={{ display: "flex", justifyContent: "center", color: "red" }}>
@@ -60,21 +37,10 @@ const UserList = () => {
             />
           </div>
         </div>
-        <div className='list'>
-          {searchResults.map(({ avatar_url, id, login }: UserProps) => (
-            <div className='card card-shadow'>
-              <input type='checkbox' onClick={() => handleClick(id)}></input>
-              <div className='card-avatar'>
-                <img src={avatar_url} alt='avatar' />
-              </div>
-              <div className='card-body'>{id}</div>
-              <div className='card-body'>{login}</div>
-              <div className='card-footer'>
-                <button className='btn'>View profile</button>
-              </div>
-            </div>
-          ))}
-        </div>
+        <Card
+          selectedUsers={selectedUsers}
+          setSelectedUsers={setSelectedUsers}
+        />
       </div>
     </>
   );
