69b79e6f176ada85c6a7f7fbf2f61cb77530ef5e
Doriane <berdalado@gmail.com>
2022-10-12 17:12:19 +0200

    optimise fetchapi

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -18,30 +18,36 @@ export const UserContextProvider = ({ children }: any) => {
   const [searchResults, setSearchResults] = useState<[]>([]);
   const [errors, setErrors] = useState("");
 
-  const getUser = async () => {
-    const controller = new AbortController();
-    const signal = controller.signal;
-    fetch(`https://api.github.com/search/users?q="${query}"`, { signal })
-      .then((res) => res.json())
-      .then((data) => {
-        setSearchResults(data.items);
-      })
-      .catch((err) => {
-        if (err.name === "AbortError") {
-          console.log("resquest cancelled");
-        } else {
-          setErrors("autre erreur");
-        }
-      });
-    // alert("this is the alert");
+  const getUser = () => {
+    if (query !== "") {
+      const controller = new AbortController();
+      const signal = controller.signal;
+      fetch(`https://api.github.com/search/users?q="${query}"`, { signal })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.message) {
+            setErrors(data.message);
+          }
+          setSearchResults(data.items);
+        })
+        .catch((err) => {
+          if (err.name === "AbortError") {
+            console.log("resquest cancelled");
+          } else {
+            console.log(err);
+          }
+        });
 
-    return () => {
-      controller.abort();
-    };
+      return () => {
+        controller.abort();
+      };
+    }
+    //si searchbar empty reinitialise to empty
+    setSearchResults([]);
   };
 
   useEffect(() => {
-    getUser();
+    return getUser();
   }, [query]);
 
   useEffect(() => console.log("searchResults", searchResults), [searchResults]);
