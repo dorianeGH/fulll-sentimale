df6f523e2465ae414f10f37d076641260e148768
Doriane <berdalado@gmail.com>
2022-10-13 17:06:38 +0200

    algo ok

---
+++ Algo/algo.js
@@ -0,0 +1,15 @@
+const fizzBuzz = (n) => {
+  let firstString, secondString, result;
+  n % 3 === 0 ? (firstString = "fizz") : (firstString = "");
+  n % 5 === 0 ? (secondString = "buzz") : (secondString = "");
+  firstString === "" && secondString === ""
+    ? (result = n)
+    : (result = firstString + secondString);
+  return result;
+};
+
+console.log(fizzBuzz(1));
+console.log(fizzBuzz(2));
+console.log(fizzBuzz(3));
+console.log(fizzBuzz(5));
+console.log(fizzBuzz(15));
