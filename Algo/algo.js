1076607a35f582bfc2d163b88cd9e780fcf296dc
Doriane <berdalado@gmail.com>
2022-10-19 09:40:42 +0200

    algo update

--- Algo/algo.js
+++ Algo/algo.js
@@ -2,17 +2,21 @@ const fizzBuzz = (n) => {
   let firstString, secondString, result;
   if (n % 3 === 0) {
     firstString = "fizz";
+  } else {
+    firstString = "";
   }
   if (n % 5 === 0) {
     secondString = "buzz";
+  } else {
+    secondString = "";
   }
   firstString === "" && secondString === ""
     ? (result = n)
     : (result = firstString + secondString);
   return result;
 };
-/* //Ternaire
-const fizzBuzz = (n) => {
+//Ternaire
+/* const fizzBuzz = (n) => {
   let firstString, secondString, result;
   n % 3 === 0 ? (firstString = "fizz") : (firstString = "");
   n % 5 === 0 ? (secondString = "buzz") : (secondString = "");
@@ -22,8 +26,19 @@ const fizzBuzz = (n) => {
   return result;
 }; */
 
+/* const fizzBuzz = (n) => {
+  if (n % 15 === 0) {
+    return "Fizzbuzz";
+  } else if (n % 3 === 0) {
+    return "Fizz";
+  } else if (n % 5 === 0) {
+    return "Buzz";
+  }
+  return n;
+}; */
+
 console.log(fizzBuzz(1));
 console.log(fizzBuzz(2));
 console.log(fizzBuzz(3));
 console.log(fizzBuzz(5));
-console.log(fizzBuzz(15));
+console.log(fizzBuzz(30));
